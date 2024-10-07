dae1131d5eb5917b5658c20dbb2539f0c244598b
thuy2020 <47225333+thuy2020@users.noreply.github.com>
2024-10-07 12:34:53 -0400

    modify growth data for time series tool

--- src/data_processing/processing_for_data_tool.R
+++ src/data_processing/processing_for_data_tool.R
@@ -13,30 +13,31 @@ city_data_temp <- read_csv("output/top100_cities.csv") %>%
 school_data_temp <- read_csv("output/top100_sd.csv")%>% 
   filter(year != 2023) 
 
-
+#function to clean up data
+modify_data <- function(data_temp){
+  data_temp|>
+    mutate(name = str_to_title(name)) |>
+    mutate(
+      modified_revenues = revenues - (expenses + current_liabilities),
+      net_pension_liab =  net_pension_liability - net_pension_assets,
+      net_opeb_liab = net_opeb_liability - net_opeb_assets
+    ) |>
+    select(-net_pension_liability, -net_opeb_liability, -net_pension_assets, -net_opeb_assets) |>
+    rename(
+      net_pension_liability = net_pension_liab,
+      net_opeb_liability = net_opeb_liab
+    ) |>
+    mutate(
+      assets_liab_ratio = total_liabilities / total_assets
+    ) |>
+    arrange(year, state.name) |>
+    select(-`...1`)
+}
 ############################
 ####### State Data ########
 ############################
 
-# clean up data
-state_data <- state_data_temp |>
-  #select(-1) |>
-  mutate(name = str_to_title(name)) |>
-  mutate(
-    modified_revenues = revenues - (expenses + current_liabilities),
-    net_pension_liab =  net_pension_liability - net_pension_assets,
-    net_opeb_liab = net_opeb_liability - net_opeb_assets
-  ) |>
-  select(-net_pension_liability, -net_opeb_liability, -net_pension_assets, -net_opeb_assets) |>
-  rename(
-    net_pension_liability = net_pension_liab,
-    net_opeb_liability = net_opeb_liab
-  ) |>
-  mutate(
-    assets_liab_ratio = total_liabilities / total_assets
-  ) |>
-  arrange(year, state.name) |>
-  select(-`...1`)
+state_data <- modify_data(state_data_temp) 
 
 # Filter 2022 data 
 state_data_22 <- state_data |>
@@ -48,25 +49,50 @@ state_data_20 <- state_data |>
   filter(year == 2020) |>
   rename_with(~ paste0(., "_20"))
 
+#growth function
+data_growth <- function(data_22, data_20) {
 
-state_data_growth <- state_data_22 |>
-  left_join(state_data_20, by = c("name_22" = "name_20", "state.name_22" = "state.name_20")) |>
+data_22 |>
+  left_join(data_20, by = c("name_22" = "name_20", "state.name_22" = "state.name_20")) |>
   mutate(
-    total_liabilities_growth = total_liabilities_22 / total_liabilities_20 - 1,
-    current_liabilities_growth = current_liabilities_22 / current_liabilities_20 - 1,
-    expenses_growth = expenses_22 / expenses_20 - 1,
-    net_opeb_liability_growth = net_opeb_liability_22 / net_opeb_liability_20 - 1,
-    net_pension_liability_growth = net_pension_liability_22 / net_pension_liability_20 - 1,
-    revenues_growth = revenues_22 / revenues_20 - 1,
-    total_assets_growth = total_assets_22 / total_assets_20 - 1,
-    assets_liab_ratio_growth = assets_liab_ratio_22 / assets_liab_ratio_20 - 1
-  ) |>
-  mutate(
+    total_liabilities_growth = case_when(
+      total_liabilities_22 == 0 & total_liabilities_20 == 0 ~ 0,
+      TRUE ~ (total_liabilities_22 - total_liabilities_20) / abs(total_liabilities_20)
+    ),
+    current_liabilities_growth = case_when(
+      current_liabilities_22 == 0 & current_liabilities_20 == 0 ~ 0,
+      TRUE ~ (current_liabilities_22 - current_liabilities_20) / abs(current_liabilities_20)
+    ),
+    expenses_growth = case_when(
+      expenses_22 == 0 & expenses_20 == 0 ~ 0,
+      TRUE ~ (expenses_22 - expenses_20) / abs(expenses_20)
+    ),
+    net_opeb_liability_growth = case_when(
+      net_opeb_liability_22 == 0 & net_opeb_liability_20 == 0 ~ 0,
+      TRUE ~ (net_opeb_liability_22 - net_opeb_liability_20) / abs(net_opeb_liability_20)
+    ),
+    net_pension_liability_growth = case_when(
+      net_pension_liability_22 == 0 & net_pension_liability_20 == 0 ~ 0,
+      TRUE ~ (net_pension_liability_22 - net_pension_liability_20) / abs(net_pension_liability_20)
+    ),
+    revenues_growth = case_when(
+      revenues_22 == 0 & revenues_20 == 0 ~ 0,
+      TRUE ~ (revenues_22 - revenues_20) / abs(revenues_20)
+    ),
+    total_assets_growth = case_when(
+      total_assets_22 == 0 & total_assets_20 == 0 ~ 0,
+      TRUE ~ (total_assets_22 - total_assets_20) / abs(total_assets_20)
+    ),
+    assets_liab_ratio_growth = case_when(
+      assets_liab_ratio_22 == 0 & assets_liab_ratio_20 == 0 ~ 0,
+      TRUE ~ (assets_liab_ratio_22 - assets_liab_ratio_20) / abs(assets_liab_ratio_20)
+    ),
     modified_revenues_20 = revenues_20 - (expenses_20 + current_liabilities_20),
     modified_revenues_22 = revenues_22 - (expenses_22 + current_liabilities_22),
-  ) |>
-  mutate(
-    modified_revenues_growth = modified_revenues_22 / modified_revenues_20 - 1
+    modified_revenues_growth = case_when(
+      modified_revenues_22 == 0 & modified_revenues_20 == 0 ~ 0,
+      TRUE ~ (modified_revenues_22 - modified_revenues_20) / abs(modified_revenues_20)
+    )
   ) |>
   rename(
     name = name_22,
@@ -74,11 +100,13 @@ state_data_growth <- state_data_22 |>
     state.name = state.name_22,
     state.abb = state.abb_22
   ) |>
-  select(-geo_id_20) 
+  select(-geo_id_20)  
 
+}
+
+state_data_growth <- data_growth(state_data_22, state_data_20) %>% 
 
 # join partisan lean
-state_data_growth <- state_data_growth |>
   left_join(partisan_lean, by = c("name" = "state")) 
 
 state_data_growth <- state_data_growth |>
@@ -106,25 +134,8 @@ write(state_data_json, file="output/state_data.json")
 ############################
 
 # clean up data
-county_data <- county_data_temp |>
-  #select(-1) |>
-  mutate(name = str_to_title(name)) |>
-  mutate(name = paste(name, `state.abb`, sep = ", ")) |>
-  mutate(
-    modified_revenues = revenues - (expenses + current_liabilities),
-    net_pension_liab =  net_pension_liability - net_pension_assets,
-    net_opeb_liab = net_opeb_liability - net_opeb_assets
-  ) |>
-  select(-net_pension_liability, -net_opeb_liability, -net_pension_assets, -net_opeb_assets) |>
-  rename(
-    net_pension_liability = net_pension_liab,
-    net_opeb_liability = net_opeb_liab
-  ) |>
-  mutate(
-    assets_liab_ratio = total_liabilities / total_assets
-  ) |>
-  arrange(year, state.name) |>
-  select(-`...1`)
+county_data <- modify_data(county_data_temp) %>% 
+mutate(name = paste(name, `state.abb`, sep = ", "))
 
 county_data_22 <- county_data |>
   filter(year == 2022) |>
@@ -135,38 +146,22 @@ county_data_20 <- county_data |>
   rename_with(~ paste0(., "_20"))
 
 
-county_data_growth <- county_data_22 |>
-  left_join(county_data_20, by = c("name_22" = "name_20", "state.name_22" = "state.name_20")) |>
-  mutate(
-    total_liabilities_growth = total_liabilities_22 / total_liabilities_20 - 1,
-    current_liabilities_growth = current_liabilities_22 / current_liabilities_20 - 1,
-    expenses_growth = expenses_22 / expenses_20 - 1,
-    net_opeb_liability_growth = net_opeb_liability_22 / net_opeb_liability_20 - 1,
-    net_pension_liability_growth = net_pension_liability_22 / net_pension_liability_20 - 1,
-    revenues_growth = revenues_22 / revenues_20 - 1,
-    total_assets_growth = total_assets_22 / total_assets_20 - 1,
-    assets_liab_ratio_growth = assets_liab_ratio_22 / assets_liab_ratio_20 - 1
-  ) |>
-  mutate(
-    modified_revenues_20 = revenues_20 - (expenses_20 + current_liabilities_20),
-    modified_revenues_22 = revenues_22 - (expenses_22 + current_liabilities_22),
-  ) |>
-  mutate(
-    modified_revenues_growth = modified_revenues_22 / modified_revenues_20 - 1
-  ) |>
-  rename(
-    name = name_22,
-    geo_id = geo_id_22,
-    state.name = state.name_22,
-    state.abb = state.abb_22
-  ) |>
-  select(-geo_id_20) |>
+county_data_growth <- data_growth(county_data_22, county_data_20) %>% 
   mutate(pct_urban_pop_22_normalized = rescale(pct_urban_pop_22, to = c(-50, 50))) |>
   mutate(median_hh_income_normalized = rescale(median_hh_income_21_22, to = c(-50, 50)))
 
 
 county_data_growth_json <- toJSON(county_data_growth, auto_unbox = TRUE, pretty = TRUE)
 write(county_data_growth_json, file="output/county_data_growth.json")
+
+county_data_growth <- county_data_growth |>
+  mutate(
+    total_liabilities_growth = total_liabilities_growth * 100,
+    net_pension_liability_growth = net_pension_liability_growth * 100,
+    expenses_growth = expenses_growth * 100
+  )
+
+write_csv(county_data_growth, "output/county_data_growth.csv")
 
 county_data_json <- toJSON(county_data, auto_unbox = TRUE, pretty = TRUE)
 write(county_data_json, file="output/county_data.json")
@@ -177,27 +172,9 @@ write(county_data_json, file="output/county_data.json")
 ############################
 
 # clean up data
-city_data <- city_data_temp |>
-  #select(-1) |>
-  mutate(name = str_to_title(name)) |>
+city_data <- modify_data(city_data_temp) %>% 
   mutate(name_state = paste(name, `state.name`, sep = ", ")) |>
-  mutate(name = paste(name, `state.abb`, sep = ", ")) |>
-  mutate(
-    modified_revenues = revenues - (expenses + current_liabilities),
-    net_pension_liab =  net_pension_liability - net_pension_assets,
-    net_opeb_liab = net_opeb_liability - net_opeb_assets
-  ) |>
-  select(-net_pension_liability, -net_opeb_liability, -net_pension_assets, -net_opeb_assets) |>
-  rename(
-    net_pension_liability = net_pension_liab,
-    net_opeb_liability = net_opeb_liab
-  ) |>
-  mutate(
-    assets_liab_ratio = total_liabilities / total_assets
-  ) |>
-  arrange(year, state.name) |>
-  select(-`...1`)
-
+  mutate(name = paste(name, `state.abb`, sep = ", ")) 
 
 city_data_22 <- city_data |>
   filter(year == 2022) |>
@@ -208,33 +185,9 @@ city_data_20 <- city_data |>
   rename_with(~ paste0(., "_20"))
 
 
-city_data_growth <- city_data_22 |>
-  left_join(city_data_20, by = c("name_22" = "name_20", "state.name_22" = "state.name_20")) |>
-  mutate(
-    total_liabilities_growth = total_liabilities_22 / total_liabilities_20 - 1,
-    current_liabilities_growth = current_liabilities_22 / current_liabilities_20 - 1,
-    expenses_growth = expenses_22 / expenses_20 - 1,
-    net_opeb_liability_growth = net_opeb_liability_22 / net_opeb_liability_20 - 1,
-    net_pension_liability_growth = net_pension_liability_22 / net_pension_liability_20 - 1,
-    revenues_growth = revenues_22 / revenues_20 - 1,
-    total_assets_growth = total_assets_22 / total_assets_20 - 1,
-    assets_liab_ratio_growth = assets_liab_ratio_22 / assets_liab_ratio_20 - 1
-  ) |>
-  mutate(
-    modified_revenues_20 = revenues_20 - (expenses_20 + current_liabilities_20),
-    modified_revenues_22 = revenues_22 - (expenses_22 + current_liabilities_22),
-  ) |>
-  mutate(
-    modified_revenues_growth = modified_revenues_22 / modified_revenues_20 - 1
-  ) |>
-  rename(
-    name = name_22,
-    geo_id = geo_id_22,
-    state.name = state.name_22,
-    state.abb = state.abb_22,
-    name_state = name_state_22
-  ) |>
-  select(-geo_id_20)
+city_data_growth <- data_growth(city_data_22, city_data_20) %>% 
+  rename(name_state = name_state_22)
+  
 
 cityCoordinates <- readRDS("data/cityCoordinates.RDS")
 
@@ -245,12 +198,20 @@ city_data_growth <- city_data_growth |>
                              "San Francisco, California",
                              name_state)) |>
   left_join(cityCoordinates, by = c("name_state" = "name_state"))|>
-  # mutate(pct_urban_pop_22_normalized = rescale(pct_urban_pop_22, to = c(-50, 50))) |>
   mutate(median_hh_income_normalized = rescale(median_hh_income_21_22, to = c(-50, 50)))
 
 
 city_data_growth_json <- toJSON(city_data_growth, auto_unbox = TRUE, pretty = TRUE)
 write(city_data_growth_json, file="output/city_data_growth.json")
+
+city_data_growth <- city_data_growth |>
+  mutate(
+    total_liabilities_growth = total_liabilities_growth * 100,
+    net_pension_liability_growth = net_pension_liability_growth * 100,
+    expenses_growth = expenses_growth * 100
+  )
+
+write_csv(city_data_growth, "output/city_data_growth.csv")
 
 city_data_json <- toJSON(city_data, auto_unbox = TRUE, pretty = TRUE)
 write(city_data_json, file="output/city_data.json")
@@ -261,25 +222,10 @@ write(city_data_json, file="output/city_data.json")
 ############################
 
 # clean up data
-school_data <- school_data_temp |>
-  #select(-1) |>
-  mutate(name = str_to_title(name)) |>
+school_data <- modify_data(school_data_temp) %>% 
   mutate(city = str_to_title(tolower(city))) |>
-  mutate(name_state = paste(city, `state.abb`, sep = ", ")) |>
-  mutate(
-    net_pension_liab = net_pension_liability - net_pension_assets,
-    net_opeb_liab = net_opeb_liability - net_opeb_assets
-  ) |>
-  select(-net_pension_liability, -net_opeb_liability, -net_pension_assets, -net_opeb_assets) |>
-  rename(
-    net_pension_liability = net_pension_liab,
-    net_opeb_liability = net_opeb_liab
-  ) |>
-  mutate(
-    assets_liab_ratio = total_liabilities / total_assets
-  ) |>
-  arrange(year, state.name) |>
-  select(-`...1`)
+  mutate(name_state = paste(city, `state.abb`, sep = ", ")) 
+
 
 school_data_22 <- school_data |>
   filter(year == 2022) |>
@@ -293,13 +239,34 @@ school_data_20 <- school_data |>
 school_data_growth <- school_data_22 |>
   left_join(school_data_20, by = c("ncesID_22" = "ncesID_20")) |>
   mutate(
-    total_liabilities_growth = total_liabilities_22 / total_liabilities_20 - 1,
-    expenses_growth = expenses_22 / expenses_20 - 1,
-    net_opeb_liability_growth = net_opeb_liability_22 / net_opeb_liability_20 - 1,
-    net_pension_liability_growth = net_pension_liability_22 / net_pension_liability_20 - 1,
-    revenues_growth = revenues_22 / revenues_20 - 1,
-    total_assets_growth = total_assets_22 / total_assets_20 - 1,
-    assets_liab_ratio_growth = assets_liab_ratio_22 / assets_liab_ratio_20 - 1
+    total_liabilities_growth = case_when(
+      total_liabilities_22 == 0 & total_liabilities_20 == 0 ~ 0,
+      TRUE ~ (total_liabilities_22 - total_liabilities_20) / abs(total_liabilities_20)
+    ),
+    expenses_growth = case_when(
+      expenses_22 == 0 & expenses_20 == 0 ~ 0,
+      TRUE ~ (expenses_22 - expenses_20) / abs(expenses_20)
+    ),
+    net_opeb_liability_growth = case_when(
+      net_opeb_liability_22 == 0 & net_opeb_liability_20 == 0 ~ 0,
+      TRUE ~ (net_opeb_liability_22 - net_opeb_liability_20) / abs(net_opeb_liability_20)
+    ),
+    net_pension_liability_growth = case_when(
+      net_pension_liability_22 == 0 & net_pension_liability_20 == 0 ~ 0,
+      TRUE ~ (net_pension_liability_22 - net_pension_liability_20) / abs(net_pension_liability_20)
+    ),
+    revenues_growth = case_when(
+      revenues_22 == 0 & revenues_20 == 0 ~ 0,
+      TRUE ~ (revenues_22 - revenues_20) / abs(revenues_20)
+    ),
+    total_assets_growth = case_when(
+      total_assets_22 == 0 & total_assets_20 == 0 ~ 0,
+      TRUE ~ (total_assets_22 - total_assets_20) / abs(total_assets_20)
+    ),
+    assets_liab_ratio_growth = case_when(
+      assets_liab_ratio_22 == 0 & assets_liab_ratio_20 == 0 ~ 0,
+      TRUE ~ (assets_liab_ratio_22 - assets_liab_ratio_20) / abs(assets_liab_ratio_20)
+    )
   ) |>
   rename(
     name = name_22,
@@ -309,13 +276,18 @@ school_data_growth <- school_data_22 |>
     name_state = name_state_22
   )
 
-#schoolCoordinates <- readRDS("schoolCoordinates.RDS")
-#schoolCoordinates <- schoolCoordinates[, 3:5]
-#school_data_growth <- school_data_growth |>
-# left_join(schoolCoordinates, by = c("name_state" = "name_state"))
 
 school_data_growth_json <- toJSON(school_data_growth, auto_unbox = TRUE, pretty = TRUE)
 write(school_data_growth_json, file="output/school_data_growth.json")
+
+school_data_growth <- school_data_growth |>
+  mutate(
+    total_liabilities_growth = total_liabilities_growth * 100,
+    net_pension_liability_growth = net_pension_liability_growth * 100,
+    expenses_growth = expenses_growth * 100
+  )
+
+write_csv(school_data_growth, "output/school_data_growth.csv")
 
 school_data_json <- toJSON(school_data, auto_unbox = TRUE, pretty = TRUE)
 write(school_data_json, file="output/school_data.json")
