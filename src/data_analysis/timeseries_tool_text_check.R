c1789f590bd80e0de2c5795f8d3a40c2fdf89c35
thuy2020 <47225333+thuy2020@users.noreply.github.com>
2024-11-19 11:06:49 -0500

    fix bug in timeseries tool text check, weighted calcualation

--- src/data_analysis/timeseries_tool_text_check.R
+++ src/data_analysis/timeseries_tool_text_check.R
@@ -94,8 +94,6 @@ write_csv(city_data_summary, "output/data_validation/city_data_summary.csv")
 #School Data Summary
 school_data_summary <- school_data_temp |>
   filter(year != 2023) |>
-  #drop_na(net_opeb_assets, net_opeb_liability, net_pension_assets, net_pension_liability,
-   #       expenses, total_liabilities, revenues, total_assets) |>
   mutate(enrollment = case_when(
     year == 2020 ~ enrollment_20,
     year == 2021 ~ enrollment_21,
@@ -116,12 +114,12 @@ school_data_summary <- school_data_temp |>
     revenues_sum = sum(revenues, na.rm = TRUE),
     total_assets_sum = sum(total_assets, na.rm = TRUE),
     debt_ratio = total_liabilities_sum / total_assets_sum,
-    net_net_pension_liability_pc = net_net_pension_liability_sum / sum(enrollment[!is.na(net_net_pension_liability_sum)], na.rm = TRUE),
-    net_net_opeb_liability_pc = net_net_opeb_liability_sum / sum(enrollment[!is.na(net_net_opeb_liability_sum)], na.rm = TRUE),
-    expenses_pc = expenses_sum / sum(enrollment[!is.na(expenses_sum)], na.rm = TRUE),
-    total_liabilities_pc = total_liabilities_sum / sum(enrollment[!is.na(total_liabilities_sum)], na.rm = TRUE),
-    revenues_pc = revenues_sum / sum(enrollment[!is.na(revenues_sum)], na.rm = TRUE),
-    total_assets_pc = total_assets_sum / sum(enrollment[!is.na(total_assets_sum)], na.rm = TRUE)
+    net_net_pension_liability_pc = net_net_pension_liability_sum / sum(enrollment[!is.na(net_net_pension_liability)], na.rm = TRUE),
+    net_net_opeb_liability_pc = net_net_opeb_liability_sum / sum(enrollment[!is.na(net_net_opeb_liability)], na.rm = TRUE),
+    expenses_pc = expenses_sum / sum(enrollment[!is.na(expenses)], na.rm = TRUE),
+    total_liabilities_pc = total_liabilities_sum / sum(enrollment[!is.na(total_liabilities)], na.rm = TRUE),
+    revenues_pc = revenues_sum / sum(enrollment[!is.na(revenues)], na.rm = TRUE),
+    total_assets_pc = total_assets_sum / sum(enrollment[!is.na(total_assets)], na.rm = TRUE)
   ) |>
   select(
     year,
@@ -140,6 +138,9 @@ school_data_summary <- school_data_temp |>
     total_assets_pc
   ) |>
   rename_with(~ str_replace(., "_sum", ""), ends_with("_sum"))
+
+
+#####
 
 write_csv(school_data_temp, "output/data_validation/school_data_temp.csv")
 write_csv(school_data_summary, "output/data_validation/school_data_summary.csv")
